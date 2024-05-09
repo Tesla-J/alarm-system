d8477e67c2d201c2d29af40df42ebc006e8af078
Rafael Marcos <rafix995@gmail.com>
2024-05-09 22:05:46 +0100

    Backbone basic structure

--- app/Backbone.py
+++ app/Backbone.py
@@ -3,4 +3,20 @@ class Backbone:
     # TODO set contants for sensors flags (don't forget alarm)
 
 
-    def 
+    # Turn off lights
+    def turn_off_button():
+        # TODO
+        pass
+
+
+    # Turn off buzzer
+    def turn_off_buzzer():
+        # TODO
+        pass
+
+    
+    # Returns a dictionary containing sensor data
+    # The sensor data includes: flag, data
+    def get_data():
+        # TODO 
+        pass
