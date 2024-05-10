5f9ecdc6dca2d55b0ff336d40a5a9a9194d7725f
Rafael Marcos <rafix995@gmail.com>
2024-05-10 04:12:24 +0100

    Backbone created: It contains all operations of the shared library and handles serial communication data

--- app/Backbone.py
+++ app/Backbone.py
@@ -1,22 +1,53 @@
+from SerialWrapper import SerialWrapper as Serial
+import ctypes
+
 class Backbone:
-    # TODO set constants for sensors working ranges
-    # TODO set contants for sensors flags (don't forget alarm)
+    # importing shared library
+    flag_lib = ctypes.CDLL('./flag.so')
+
+    def __init__(self):
+        self.serial = Serial()
 
 
-    # Turn off lights
-    def turn_off_button():
-        # TODO
-        pass
+    def assign_flag(self, flag, data):
+        fun = flag_lib.assign_data
+        fun.args = [ctypes.c_int, ctypes.c_int]
+        fun.restype = ctypes.c_int
+
+        return fun(flag, data)
 
 
-    # Turn off buzzer
-    def turn_off_buzzer():
-        # TODO
-        pass
+    def remove_flag(self, data):
+        fun = flag_lib.remove_flag
+        fun.args = [ctypes.c_int]
+        fun.restype = ctypes.c_int
+
+        return fun(data)
 
     
-    # Returns a dictionary containing sensor data
-    # The sensor data includes: flag, data
-    def get_data():
-        # TODO 
-        pass
+    def read_flag(self, data):
+        fun = flag_lib.read_flag
+        fun.args = [ctypes.c_int]
+        fun.restype = ctypes.c_int
+
+        return fun(data)
+
+
+    # easily access flag values flags
+    def flag_value(self, flag_name):
+        return ctypes.c_int.in_dll(flag_lib, flag_name).value
+
+    
+    # Returns a tuple containing sensor data
+    # The sensor data contains: flag and data
+    def get_data(self):
+        data = self.serial.read()
+        flag = self.read_flag(data)
+        data = self.remove_flag(data)
+
+        return (flag, data)
+
+
+    def send_data(self, flag, data):
+        data = self.assign_flag(flag, data)
+        self.serial.write(data)
