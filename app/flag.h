a552c546cf36e9e5e50fba78e9dd8ac89a1fca83
Rafael Marcos <rafix995@gmail.com>
2024-05-10 01:09:28 +0100

    flag.h updated: #define contantes changed to const

--- app/flag.h
+++ app/flag.h
@@ -7,15 +7,15 @@
  * They will be in the 4 most significant bits
  * to standardize the serial communication
  */
-#define TEMPERATURE_SENSOR	1
-#define LIGHT_SENSOR		2
-#define GAS_SENSOR		3
+const int TEMPERATURE_SENSOR	=	1;
+const int LIGHT_SENSOR		=	2;
+const int GAS_SENSOR		=	3;
 
-#define FIRE_ALERT		4
-#define LIGHTS_ON		5
-#define TURN_OFF_LIGHS		6
-#define BUZZER_ON		7
-#define TURN_OFF_BUZZER		8
+const int FIRE_ALERT		=	4;
+const int LIGHTS_ON		=	5;
+const int TURN_OFF_LIGHS	=	6;
+const int BUZZER_ON		=	7;
+const int TURN_OFF_BUZZER	=	8;
 
 /*
  * Sensor manipulation functions
